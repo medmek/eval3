938df58fa0e9e9711cbe1291716030f6c2d221b3
medmek <mekkaoui.mpr@gmail.com>
2016-05-27 18:12:47 +0100

    Dernier commit avant livrable 1 front

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,48 @@
 
 
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp',['ngRoute']);
 
-myApp.controller('MainCtrl', function($scope){
-    $scope.message = "Accueil";
+myApp.controller('MainCtrl', function($rootScope){
+    //code
+     $rootScope.title = "Accueil";
+
+});
+
+myApp.controller('AccueilCtrl', function($rootScope){
+    $rootScope.title = "Accueil";
+    
+});
+
+myApp.controller('ProjetsCtrl', function($rootScope){
+    $rootScope.title = "Tableau de Bord";
+    
+});
+
+myApp.controller('AjoutCtrl', function($rootScope){
+    $rootScope.title = "Import de Projet";
+    
+});
+
+myApp.config( function( $routeProvider ) {
+	$routeProvider
+	.when('/', {
+                templateUrl : 'accueil.html',
+                controller  : 'AccueilCtrl'
+    })
+	.when('/projets', {
+						templateUrl : 'projets.html',
+                		controller  : 'ProjetsCtrl'
+
+	})
+	.when('/projets/ajout', {
+						templateUrl : 'projets_ajout.html',
+                		controller  : 'AjoutCtrl'
+
+	})
+	.when('/profil', {
+						templateUrl : 'profil.html',
+                		controller  : 'ProjetsCtrl'
+
+	})
+	.otherwise({redirectTo: '/'});
 });
